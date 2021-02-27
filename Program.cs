275161f599ec5d259a02301cd82df88d8bbc33d8
Chengkai Yang <yangchengkai@gmail.com>
2021-02-27 11:58:10 +0100

    sp for KV access

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Azure.Identity;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -37,6 +38,14 @@ namespace AzureAppConfigurationTest
 
                                                                                          //Setup feature
                                                                                          az.UseFeatureFlags();
+
+                                                                                         //Setup service principle to access key vault
+                                                                                         Environment.SetEnvironmentVariable("AZURE_CLIENT_ID ", "971c306c-8ea5-4247-8a07-7732facc8d58");
+                                                                                         Environment.SetEnvironmentVariable("AZURE_CLIENT_SECRET", "UQF-z_kFJRr~lCAWrwHslg0f1Q75-4rvMw");
+                                                                                         Environment.SetEnvironmentVariable("AZURE_TENANT_ID", "4e6f57dc-a3d9-4a0c-818b-a7c1bb2b79f6");
+                                                                                         az.ConfigureKeyVault(kv => {
+                                                                                            kv.SetCredential(new DefaultAzureCredential());
+                                                                                         });
                                                                                       });
                                                                                 });
                                            webBuilder.UseStartup<Startup>();
